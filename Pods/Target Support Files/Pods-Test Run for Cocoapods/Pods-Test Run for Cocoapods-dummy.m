4c14b9a13fe35bb489c6267d8d0ab6be3624eb4c
Limoncilla <limoncilla@Limoncillas-Mac.local>
2016-02-07 17:18:42 -0800

    part 22

---
+++ Pods/Target Support Files/Pods-Test Run for Cocoapods/Pods-Test Run for Cocoapods-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Test_Run_for_Cocoapods : NSObject
+@end
+@implementation PodsDummy_Pods_Test_Run_for_Cocoapods
+@end
---
+++ Pods/Target Support Files/Pods-Test Run for Cocoapods/Pods-Test Run for Cocoapods-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Test_Run_for_CocoapodsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Test_Run_for_CocoapodsVersionString[];
+
